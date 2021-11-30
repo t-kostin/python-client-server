809e454d6066df09059b27f2471359b0386ad495
t-kostin <timur.kostin@gmail.com>
2021-11-30 18:20:55 +0300

    lesson 5 completed

---
+++ messenger/modules/constants.py
@@ -0,0 +1,13 @@
+BUFFER_SIZE = 4096
+
+OK200 = 200
+ERR400 = 400
+ERR402 = 402
+
+INVALID_JSON = 'Invalid JSON format'
+BAD_TIMESTAMP = 'Bad timestamp'
+NO_STATUS = 'No status provided'
+INVALID_ACTION = 'Invalid action'
+WRONG_USER = 'Wrong account name'
+
+PRESENCE = 'presence'
---
+++ messenger/tests/test_send_receive_message.py
@@ -0,0 +1,61 @@
+import unittest
+from ..modules.messenger import Jim, BUFFER_SIZE
+
+
+class TestSendReceiveMessage(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """
+        Создаем класс Socket с методами send и recv
+        для тестирования
+
+        """
+
+        class Socket:
+
+            def __init__(self):
+                self.buffer = b''
+
+            def send(self, data: bytes) -> None:
+                self.buffer = data
+
+            def recv(self, buffer_size: int) -> bytes:
+                return self.buffer[:buffer_size]
+
+        self.socket = Socket()
+        self.server = Jim()
+
+    def test_send_receive(self):
+        message = {
+            'action': 'presence',
+            'time': self.server.current_timestamp,
+            'type': 'status',
+            'user': {
+                'account_name': 'guest',
+                'status': 'I am busy!',
+            },
+        }
+        self.server.send_message(self.socket, message)
+        self.assertEqual(self.server.receive_message(self.socket), message)
+
+    def test_very_long_message(self):
+        message = {
+            'action': 'presence',
+            'time': self.server.current_timestamp,
+            'type': 'status',
+            'user': {
+                'account_name': 'usr' * BUFFER_SIZE,
+                'status': 'I am busy!',
+            },
+        }
+        self.server.send_message(self.socket, message)
+        self.assertEqual(self.server.receive_message(self.socket), {})
+
+    def test_tuple(self):
+        message = (1, 2, 3)
+        self.server.send_message(self.socket, message)
+        self.assertNotEqual(self.server.receive_message(self.socket), message)
+
+
+if __name__ == '__main__':
+    unittest.main()
